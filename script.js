72ac2dc494315bf42e8cc5ed4d7f06a6ece9c53f
Isac <isac.mamede14@gmail.com>
2023-08-22 22:22:56 -0300

    improvements

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// Pegando os botões
+// Pegando os componentes da página
 const add = document.querySelector("#btnAdd");
 const clean = document.querySelector("#btnClean");
-
-// Pegando a div que abriga os itens adicionados
 const divItem = document.querySelector(".divItens");
 const form = document.querySelector("#formItens");
 
-// Adicionando itens na lista
-add.addEventListener("click", function () {
+function adicionar() {
     if (form.value == "") {
         alert(
             "Escreva o nome do item que deseja adicionar a lista de compras!"
@@ -17,16 +14,26 @@ add.addEventListener("click", function () {
         divItem.innerHTML += `<p><li>${item}</li></p>`;
         form.value = "";
     }
-});
+}
+
+add.addEventListener("click", adicionar);
 
 // Limpando a lista de itens
 clean.addEventListener("click", function () {
     divItem.innerHTML = "";
 });
 
-// Desabilitando a tecla Enter
+// Configurando a tecla Enter (desativa o envio de formulário para não recarregar a página e executa a função adicionar)
 form.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         e.preventDefault();
+        adicionar();
+    }
+});
+
+// Marcando um item como concluído
+divItem.addEventListener("click", function (event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
     }
 });
